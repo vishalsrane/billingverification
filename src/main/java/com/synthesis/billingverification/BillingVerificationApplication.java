f0c06503e43820e13b64864849aaf482d460e6bb
Vishal Rane <vishalsrane@gmail.com>
2021-04-09 17:46:06 +0530

    first commit

---
+++ src/main/java/com/synthesis/billingverification/BillingVerificationApplication.java
@@ -0,0 +1,13 @@
+package com.synthesis.billingverification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingVerificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingVerificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/synthesis/billingverification/BillingVerificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synthesis.billingverification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingVerificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
